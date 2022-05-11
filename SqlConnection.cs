c6a5cc1f1acc3465701554e951bfe95b95805ce5
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-05-10 22:45:45 -0500

    created SqlConnection class

---
+++ SqlConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PolymorphismExercise
+{
+    public class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine("Connection closed.");
+        }
+
+        public override void OpenConnection()
+        {
+            Console.WriteLine("Connection opened.");
+        }
+    }
+}
